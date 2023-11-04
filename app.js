5cb6715d30f6195bfecf9faaf812523626a90164
lakshay.kaushik <lakshay.kaushik@jungleegames.com>
2023-11-04 21:37:13 +0530

    new change

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // const heading = React.createElement("h1", {
 //     id: "heading"
 // }, "Hello World");
-const code = React.createElement("div", {id : "parent"}, [React.createElement("div", {id : "child"}, React.createElement("h1", {id : "heading-1"}, "this is heading")), React.createElement("div", {id : "child"}, React.createElement("h1", {id : "heading-1"}, "this is heading"))]);
+const code = React.createElement("div", {id : "parent"}, [
+    React.createElement("div", {id : "child"}, 
+    React.createElement("h1", {id : "heading-1"}, "this is heading")),
+     React.createElement("div", {id : "child"},
+      React.createElement("h1", {id : "heading-1"}, "this is heading")) ]);
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(code);
