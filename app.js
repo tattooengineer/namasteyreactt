69d4b308fdc57df06c4bb36c0381266c69d4ad45
lakshay.kaushik <lakshay.kaushik@jungleegames.com>
2023-12-23 13:05:48 +0530

    new start

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const code = React.createElement("div", {id : "parent"}, [
      React.createElement("div", {id : "child"},
       React.createElement("h1", {id : "heading-1"}, "this is heading")) ]);
 
+
+
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(code);
 
