cd84f12da68515e5a667f9da217a8c7da6c8cf33
lakshay.kaushik <lakshay.kaushik@jungleegames.com>
2023-12-30 15:02:37 +0530

    new updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// const heading = React.createElement("h1", {
-//     id: "heading"
-// }, "Hello World");
+import React from "react";
+import ReactDOM from "react-dom/client";
 const code = React.createElement("div", {id : "parent"}, [
     React.createElement("div", {id : "child"}, 
     React.createElement("h1", {id : "heading-1"}, "this is heading")),
