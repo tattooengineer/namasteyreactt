3555b3c3e0939c11f086647cba1df0e6b3148fb7
lakshay.kaushik <lakshay.kaushik@jungleegames.com>
2024-01-06 22:28:55 +0530

    rohit teach

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const code = React.createElement("div", {id : "parent"}, [
-    React.createElement("div", {id : "child"}, 
-    React.createElement("h1", {id : "heading-1"}, "this is heading")),
-     React.createElement("div", {id : "child"},
-      React.createElement("h1", {id : "heading-1"}, "this is heading")) ]);
 
-
-
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(code);
+const heading = React.createElement("p",{class:"head"}, "cjhbchjdsac jdshcj bkerwfgbrgjhfberhfgehr");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
 
 
   
