87cef8e13dee65a87775aceb5a9688775efab20d
lakshay.kaushik <lakshay.kaushik@jungleegames.com>
2024-02-17 19:06:43 +0530

    lakshay is here

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("p",{class:"head"}, "cjhbchjdsac jdshcj bkerwfgbrgjhfberhfgehr");
+const heading = React.createElement("p",{class:"head"}, "Lakshay is here");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
 
